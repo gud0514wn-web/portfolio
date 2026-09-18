bfe0ec76188e811abad93c2a695d59b07e34dcca
gud0514wn-web <gud0514wn@gmail.com>
2026-09-19 01:42:23 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="portfolio-mobile-v52";
+const CACHE="portfolio-mobile-v53";
 const CORE=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
